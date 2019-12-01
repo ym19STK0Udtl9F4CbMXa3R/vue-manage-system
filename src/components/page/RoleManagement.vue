f247a6980ba30615c436daa5a6afa471b20d3e8d
Nines97 <978922493@qq.com>
2019-12-02 01:22:04 +0800

    完成角色管理相关功能

--- src/components/page/RoleManagement.vue
+++ src/components/page/RoleManagement.vue
@@ -3,7 +3,7 @@
         <div class="crumbs">
             <el-breadcrumb separator="/">
                 <el-breadcrumb-item>
-                    <i class="el-icon-lx-cascades"></i> 角色管理
+                    <i class="el-icon-user" /> 角色管理
                 </el-breadcrumb-item>
             </el-breadcrumb>
         </div>
@@ -15,12 +15,9 @@
                         class="handle-del mr10"
                         @click="delAllSelection"
                 >批量删除</el-button>
-                <el-select v-model="query.address" placeholder="地址" class="handle-select mr10">
-                    <el-option key="1" label="广东省" value="广东省"></el-option>
-                    <el-option key="2" label="湖南省" value="湖南省"></el-option>
-                </el-select>
-                <el-input v-model="query.name" placeholder="用户名" class="handle-input mr10"></el-input>
+                <el-input v-model="query.searchText" placeholder="角色名" class="handle-input mr10" />
                 <el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
+                <el-button type="primary" icon="el-icon-lx-friendadd" class="handle-del mr10" @click="handleAdd">添加新角色</el-button>
             </div>
             <el-table
                     :data="tableData"
@@ -30,31 +27,19 @@
                     header-cell-class-name="table-header"
                     @selection-change="handleSelectionChange"
             >
-                <el-table-column type="selection" width="55" align="center"></el-table-column>
-                <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
-                <el-table-column prop="name" label="用户名"></el-table-column>
-                <el-table-column label="账户余额">
-                    <template slot-scope="scope">￥{{scope.row.money}}</template>
-                </el-table-column>
-                <el-table-column label="头像(查看大图)" align="center">
-                    <template slot-scope="scope">
-                        <el-image
-                                class="table-td-thumb"
-                                :src="scope.row.thumb"
-                                :preview-src-list="[scope.row.thumb]"
-                        ></el-image>
-                    </template>
-                </el-table-column>
-                <el-table-column prop="address" label="地址"></el-table-column>
-                <el-table-column label="状态" align="center">
+                <el-table-column type="selection" width="55" align="center" />
+                <el-table-column prop="id" label="ID" width="200" align="center" />
+                <el-table-column prop="name" label="角色名" align="center" />
+                <el-table-column prop="remark" label="备注" align="center" />
+                <el-table-column label="状态" width="100" align="center">
                     <template slot-scope="scope">
                         <el-tag
-                                :type="scope.row.state==='成功'?'success':(scope.row.state==='失败'?'danger':'')"
-                        >{{scope.row.state}}</el-tag>
+                                :type="scope.row.status ? 'success': 'danger'"
+                        >{{scope.row.status ? "激活":"冻结"}}</el-tag>
                     </template>
                 </el-table-column>
 
-                <el-table-column prop="date" label="注册时间"></el-table-column>
+                <el-table-column prop="createTime" label="注册时间" align="center" />
                 <el-table-column label="操作" width="180" align="center">
                     <template slot-scope="scope">
                         <el-button
@@ -67,7 +52,7 @@
                                 icon="el-icon-delete"
                                 class="red"
                                 @click="handleDelete(scope.$index, scope.row)"
-                        >删除</el-button>
+                        >{{ scope.row.status ? "冻结":"激活" }}</el-button>
                     </template>
                 </el-table-column>
             </el-table>
@@ -75,81 +60,216 @@
                 <el-pagination
                         background
                         layout="total, prev, pager, next"
-                        :current-page="query.pageIndex"
-                        :page-size="query.pageSize"
+                        :current-page="query.current"
+                        :page-size="query.size"
                         :total="pageTotal"
                         @current-change="handlePageChange"
-                ></el-pagination>
+                />
             </div>
         </div>
 
         <!-- 编辑弹出框 -->
         <el-dialog title="编辑" :visible.sync="editVisible" width="30%">
-            <el-form ref="form" :model="form" label-width="70px">
-                <el-form-item label="用户名">
-                    <el-input v-model="form.name"></el-input>
+            <el-form ref="form" :model="editData" label-width="70px">
+                <el-form-item label="角色名">
+                    <!-- @keyup.native.enter="saveEdit" 需使用native覆盖原有封装的keyup事件即可 -->
+                    <el-input v-model="editData.name" />
                 </el-form-item>
-                <el-form-item label="地址">
-                    <el-input v-model="form.address"></el-input>
+                <el-form-item label="备注">
+                    <el-input type="textarea" rows="5" v-model="editData.remark" />
+                </el-form-item>
+                <el-form-item label="权限列表">
+                    <el-tree
+                            ref="permissionTree"
+                            :data="treeData"
+                            show-checkbox
+                            node-key="id"
+                            default-expand-all
+                            :expand-on-click-node="false">
+                    </el-tree>
                 </el-form-item>
             </el-form>
             <span slot="footer" class="dialog-footer">
-                <el-button @click="editVisible = false">取 消</el-button>
+                <el-button @click="resetForm('form')">取 消</el-button>
                 <el-button type="primary" @click="saveEdit">确 定</el-button>
+            </span>
+        </el-dialog>
+
+        <!-- 新增弹出框 -->
+        <el-dialog title="新增" :visible.sync="addVisible" width="30%">
+            <el-form ref="addRuleForm" status-icon :model="addData" :rules="rules" label-width="70px">
+                <el-form-item label="角色名" prop="name">
+                    <!-- @keyup.native.enter="saveEdit" 需使用native覆盖原有封装的keyup事件即可 -->
+                    <el-input v-model="addData.name" />
+                </el-form-item>
+                <el-form-item label="备注" prop="remark">
+                    <el-input type="textarea" rows="5" v-model="addData.remark" />
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="resetForm('addRuleForm')">取 消</el-button>
+                <el-button type="primary" @click="submitAdd('addRuleForm')">确 定</el-button>
             </span>
         </el-dialog>
     </div>
 </template>
 
 <script>
-    import { fetchData } from '../../api/index';
     export default {
         name: 'roleManagement',
         data() {
+            var treeData = [];
+            var validateName = (rule, value, callback) => {
+                // 判断value值且不是空格
+                if (!value || value.split(" ").join("").length === 0) {
+                    return callback(new Error('角色名不能为空'));
+                    // 限制用户名只能由字母+数组组成
+                }else if (!new RegExp(/^[A-Za-z0-9]+$/).test(value)){
+                    return callback(new Error('角色名只能为字母+数组组成'))
+                }else {
+                    callback();
+                }
+            };
             return {
-                query: {
-                    address: '',
-                    name: '',
-                    pageIndex: 1,
-                    pageSize: 10
+                treeData: JSON.parse(JSON.stringify(treeData)),
+                props: {
+                    label: 'name',
+                    children: 'zones'
                 },
+                count: 1,
+                // 分页查询数据
+                query: {
+                    // username查询
+                    searchText: '',
+                    // 当前页
+                    current: 1,
+                    // 每页数据大小
+                    size: 5
+                },
+                // 表单添加数据
+                addData: {
+                    username: '',
+                    nickName: '',
+                    password: ''
+                },
+                rules: {
+                    name: [
+                        { validator: validateName, trigger: 'blur' }
+                    ]
+                },
+                // 表格数据
                 tableData: [],
                 multipleSelection: [],
                 delList: [],
+                // 是否显示编辑框
                 editVisible: false,
+                // 编辑数据
+                editData:{},
+                // 是否显示新增框
+                addVisible: false,
+                // 总页数
                 pageTotal: 0,
+                // 选中权限列表
+                checkedPermissionList: [],
+                // 一行数据
                 form: {},
+                // 本行id
                 idx: -1,
                 id: -1
             };
         },
         created() {
             this.getData();
+            this.getPermissionTree();
         },
         methods: {
-            // 获取 easy-mock 的模拟数据
+            // 分页查询
             getData() {
-                fetchData(this.query).then(res => {
-                    console.log(res);
-                    this.tableData = res.list;
-                    this.pageTotal = res.pageTotal || 50;
-                });
+                this.axios.post("/sys/role/findPage", this.query).then(
+                    (response) => {
+                        // console.log(response);
+                        this.tableData = response.data.data.rows;
+                        this.pageTotal = response.data.data.total;
+                    }
+                )
+            },
+            // 获取权限树
+            getPermissionTree() {
+                this.axios.get("/sys/permission/findPermissionTree").then(
+                    (response) => {
+                        if (response.data.status === '200'){
+                            // console.log(response);
+                            this.treeData = response.data.data;
+                        }else {
+                            console.error("获取权限树失败");
+                        }
+                    }
+                );
             },
             // 触发搜索按钮
             handleSearch() {
-                this.$set(this.query, 'pageIndex', 1);
+                this.$set(this.query, 'current', 1);
                 this.getData();
+            },
+            // 添加操作
+            handleAdd() {
+                this.addVisible = true;
+            },
+            // 提交添加
+            submitAdd(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        this.axios.post("sys/role/add",this.addData).then(
+                            (response) => {
+                                if (response.data.status === '200'){
+                                    this.addVisible = false;
+                                    this.resetForm(formName);
+                                    // 刷新，重新获取分页数据
+                                    this.getData();
+                                    this.$message.success(response.data.data);
+                                }else {
+                                    this.$message.error(response.data.data);
+                                }
+                            },(error => {
+                                this.$message.error('获取数据失败');
+                            })
+                        );
+                    } else {
+                        this.$message.error('请输入正确数据后重试');
+                        console.error('error submit!!');
+                        return false;
+                    }
+                });
             },
             // 删除操作
             handleDelete(index, row) {
                 // 二次确认删除
-                this.$confirm('确定要删除吗？', '提示', {
+                this.$confirm('确定要'+ (row.status ? '冻结':'激活') +'吗？', '提示', {
                     type: 'warning'
                 })
+                    // 点击确定执行事件
                     .then(() => {
-                        this.$message.success('删除成功');
-                        this.tableData.splice(index, 1);
+                        let updateStatus;
+                        if (row.status){
+                            // 冻结
+                            updateStatus = this.axios.delete("/sys/role/" + row.id);
+                        }else {
+                            // 激活
+                            updateStatus = this.axios.put("/sys/role/" + row.id);
+                        }
+                        updateStatus.then(
+                            (response) => {
+                                if (response.data.status === '200'){
+                                    this.$message.success(response.data.data);
+                                    this.getData();
+                                    // this.tableData.splice(index, 1);
+                                }else {
+                                    this.$message.error(response.data.data);
+                                }
+                            }
+                        );
                     })
+                    // 点击取消执行事件
                     .catch(() => {});
             },
             // 多选操作
@@ -161,7 +281,7 @@
                 let str = '';
                 this.delList = this.delList.concat(this.multipleSelection);
                 for (let i = 0; i < length; i++) {
-                    str += this.multipleSelection[i].name + ' ';
+                    str += this.multipleSelection[i].username + ' ';
                 }
                 this.$message.error(`删除了${str}`);
                 this.multipleSelection = [];
@@ -170,18 +290,79 @@
             handleEdit(index, row) {
                 this.idx = index;
                 this.form = row;
+                // 获取用户信息
+                this.axios.get("/sys/role/"+ this.form.id).then(
+                    (response) => {
+                        // console.log(response);
+                        if (response.data.status === '200'){
+                            this.editData = response.data.data;
+                        }else {
+                            this.$message.error(response.data.data);
+                        }
+                    }
+                );
+                // 获取角色的权限信息
+                this.axios.get("/sys/role/findRolePermission/" + this.form.id).then(
+                    (response) => {
+                        for (var i=0; i< response.data.data.length; i++){
+                            this.checkedPermissionList.push(response.data.data[i].permissionId);
+                        }
+                        /**
+                         * default-checked-keys是初始化加载的时候用的，数据请求到的时候树组件已经加载完成了，
+                         * 这时候改变default-checked-keys的值就没有用了。
+                         * 需要通过this.$refs.tree.setCheckedKeys来设置选中节点
+                         */
+                        this.$refs.permissionTree.setCheckedKeys(this.checkedPermissionList);
+                    }
+                );
                 this.editVisible = true;
             },
             // 保存编辑
             saveEdit() {
-                this.editVisible = false;
-                this.$message.success(`修改第 ${this.idx + 1} 行成功`);
-                this.$set(this.tableData, this.idx, this.form);
+                // 更新用户信息
+                this.axios.put("/sys/role/update", this.editData).then(
+                    (response) => {
+                        // console.log(response);
+                        if (response.data.status === '200'){
+                            this.$message.success(`修改第 ${this.idx + 1} 行成功`);
+                            this.$set(this.tableData, this.idx, this.editData);
+                            this.editVisible = false;
+                        }else {
+                            this.$message.error(response.data.data);
+                        }
+                    }
+                );
+                // 更新角色权限信息
+                const updatePermissionList = [];
+                // 获取选中的ID
+                const checkedKeysList = this.$refs.permissionTree.getCheckedKeys();
+                for (var i=0; i< checkedKeysList.length; i++){
+                    updatePermissionList.push({'roleId': this.editData.id, 'permissionId': checkedKeysList[i]});
+                }
+                this.axios.post("/sys/role/modifyRolePermission", updatePermissionList).then(
+                    (response) => {
+                        if (response.data.status === '200'){
+                            this.$message.success(response.data.data);
+                        } else {
+                            this.$message.success(response.data.data);
+                        }
+                        // 清空角色数据
+                        this.checkedPermissionList = [];
+                    }
+                );
             },
             // 分页导航
             handlePageChange(val) {
-                this.$set(this.query, 'pageIndex', val);
+                this.$set(this.query, 'current', val);
                 this.getData();
+            },
+            // 清空表单
+            resetForm(formName) {
+                this.addVisible = false;
+                this.editVisible = false;
+                this.$refs[formName].resetFields();
+                // 清空权限数据
+                this.checkedPermissionList = [];
             }
         }
     };
